8e717fb3d8f22435d51f0b0d4ccadc1daac5a97d
EGAT <egacebo@yahoo.com>
2021-11-05 12:59:51 +0100

    Push_Swap - 05-11-21 12:59

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 14:22:36 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/11/05 12:43:20 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/11/05 12:59:49 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,7 +117,6 @@ int		ft_squareroot(int n);
 void	pushnumbertotop_a(t_pl *stk);
 void	free_leaks(t_pl *stk, char **split);
 void	printstacks(t_pl *stk);
-char	*ft_strdup_char(const char *str, char stop);
 int		ft_atoi_modified(const char *str, int *nbr);
 
 #endif
--- sources/ft_atoi_modified.c
+++ sources/ft_atoi_modified.c
@@ -6,58 +6,37 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:14:38 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/11/04 15:22:46 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/11/05 12:59:06 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+/* atoi pasa un puntero al número en parámetro de entrada y retorna 0 si */
+/* hay un error (no un número entero) */
 
-/*
-** copy a str until stop character or end of string str
-*/
-
-char		*ft_strdup_char(const char *str, char stop)
-{
-	int		i;
-	char	*pt;
-
-	i = 0;
-	while (str[i] && str[i] != stop)
-		i++;
-	if (!(pt = malloc((i + 1) * sizeof(char))))
-		return (0);
-	i = -1;
-	while (str[++i] && str[i] != stop)
-		pt[i] = str[i];
-	pt[i] = '\0';
-	return (pt);
-}
-
-/*
-** atoi passing a pointer to the number in parameter and returning 1 if there is
-** an error (not a number or not a integer)
-*/
-
-int			ft_atoi_m(const char *str, int *num)
+int	ft_atoi_modified(const char *str, int *nbr)
 {
 	char	*ptr;
-	long	nbr;
 	int		sign;
+	long	num;
 	size_t	i;
 
 	ptr = (char *)str;
-	nbr = 0;
+	num = 0;
 	sign = 1;
-	if (*ptr == '-' && ptr++)
-		sign *= -1;
-	if (*ptr == '\0')
-		return (1);
+	if ((*str == '-') || (*str == '+'))
+	{
+		if (*ptr == '-' && ptr++)
+			sign *= -1;
+	}
+	if ((*ptr == '\0') || (*str == '-') || (*str == '+'))
+		return (0);
 	i = 0;
 	while (*ptr >= '0' && *ptr <= '9' && i++ < 10)
-		nbr = nbr * 10 + *ptr++ - '0';
-	if (*ptr != '\0' || nbr * sign > 2147483647 || nbr * sign < -2147483648)
-		return (1);
-	*num = nbr * sign;
-	return (0);
+		num = num * 10 + *ptr++ - '0';
+	if (*ptr != '\0' || num * sign > 2147483647 || num * sign < -2147483648)
+		return (0);
+	*nbr = num * sign;
+	return (1);
 }
